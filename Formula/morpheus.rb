3215859a2209765b5722d79753674ab316e005c5
Andras Szabo <andras.szabo@iop-ventures.com>
2019-02-28 11:24:44 +0100

    formula update

--- Formula/morpheus.rb
+++ Formula/morpheus.rb
@@ -2,7 +2,7 @@ class Morpheus < Formula
   desc "IoP Morpheus demo"
   homepage "https://iop.global/"
   url "https://github.com/Internet-of-People/homebrew-morpheus/blob/master/Releases/morpheus-0.1.tar.gz"
-  sha256 "029a9c458f64a33cfd3be740998573ee935c717471861c38068b3d4824b2faac"
+  sha256 "f149d1ace19248efca5f04c3514ca8ae9599fd80b29761b175d8f451113de204"
   depends_on "botan"
   depends_on "open-scene-graph"
   depends_on "rocksdb"

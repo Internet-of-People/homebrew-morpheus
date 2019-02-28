c47608dab04b710897a87809430162cf11096790
Andras Szabo <andras.szabo@iop-ventures.com>
2019-02-28 16:45:28 +0100

    fixed build issues

--- Formula/morpheus.rb
+++ Formula/morpheus.rb
@@ -2,7 +2,7 @@ class Morpheus < Formula
   desc "IoP Morpheus demo"
   homepage "https://iop.global/"
   url "https://github.com/Internet-of-People/homebrew-morpheus/raw/master/Releases/morpheus-0.1.tar.gz"
-  sha256 "297d050dd7da52712807d5b3b32235434b65a5f9980aef98ed122464db8bf5d0"
+  sha256 "953c1f54676b7d00c9912f18484531ef08539b5a1e0c2f31e8c197a4510b5145"
   depends_on "botan"
   depends_on "open-scene-graph"
   depends_on "rocksdb"

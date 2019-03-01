c5066b290a36662b9a822355aa190773d83a20e9
Andras Szabo <andras.szabo@iop-ventures.com>
2019-03-01 14:06:41 +0100

    demo release

--- Formula/morpheus.rb
+++ Formula/morpheus.rb
@@ -4,7 +4,7 @@ class Morpheus < Formula
   desc "IoP Morpheus demo"
   homepage "https://iop.global/"
   url "https://github.com/Internet-of-People/homebrew-morpheus/raw/master/Releases/morpheus-0.1.tar.gz"
-  sha256 "953c1f54676b7d00c9912f18484531ef08539b5a1e0c2f31e8c197a4510b5145"
+  sha256 "dc88c1a01dcea1a5f14b25f3ec733f42614b7b006d0657a9b83aa9dde229f14e"
   depends_on "botan"
   depends_on "open-scene-graph"
   depends_on "rocksdb"
